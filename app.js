98513d861e0eef1f4a8aa5c4202556012a591527
fakhira <fakhira.yaqub>
2021-01-14 12:09:52 +0100

    added renamed password.routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const userRouter = require("./routes/user.routes");
-const passwordRouter = require("./routes/password.routes");
+const passwordRouter = require("./routes/auth.routes");
 const errorHandling = require("./middleware/error-handling.middleware");
 
 const app = express();
