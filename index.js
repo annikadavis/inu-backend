8abb41c98bfef4e3d180c080d490c8a9acb5cb41
Inna Zilberov <70526642+silberov@users.noreply.github.com>
2021-01-14 12:00:46 +0100

    m

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`serving on http://localhost:${PORT}`);
